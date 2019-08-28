5b0eb4235c113c11afdba746f559b9b24e3ac8ed
Mario Johar <mjohar30@gmail.com>
2019-08-28 15:08:01 -0500

    Cambios en estilos

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const celeste = document.getElementById('celeste')
 const violeta = document.getElementById('violeta')
 const naranja = document.getElementById('naranja')
 const verde = document.getElementById('verde')
-const ultimoNivel = 10
+const ultimoNivel = 10;
 
 class Juego {
     constructor(){
